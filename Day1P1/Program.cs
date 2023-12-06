203a9a6da3de364b1b1ff2220aadd28fcfcb06a6
coenbrummelhuis <cbrumleger@gmail.com>
2023-12-06 16:41:07 +0100

    Day 1 Part 1

---
+++ Day1P1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Day1
+{
+    
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            string input = File.ReadAllText("..\\..\\input.txt");
+            int total = 0;
+            foreach (string line in input.Split('\n'))
+            {
+                int linetotal = 0;
+                
+                Boolean search = true;
+                char[] lineArray = line.ToCharArray();
+                int index = 0;
+                while (search)
+                {
+                    if (Char.IsNumber(lineArray[index]))
+                    {
+                        linetotal += (int.Parse(lineArray[index].ToString()))*10;
+                        search = false;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+                search = true;
+                
+                Array.Reverse(lineArray);
+                index = 0;
+                while (search)
+                {
+                    if (Char.IsNumber(lineArray[index]))
+                    {
+                        linetotal += int.Parse(lineArray[index].ToString());
+                        search = false;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+
+                total += linetotal;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Day1P2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace Day1P2
+{
+    internal class Program
+    {
+        static string[] letters = new[]
+        {
+            "oneight": "18",
+            "twone": "21",
+            "threeight": "38",
+            "fiveight": "58",
+            "sevenine": "79",
+            "eightwo": "82",
+            "eighthree",
+            "nineight",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            
+        };
+        public static void Main(string[] args)
+        {
+            string input = File.ReadAllText("../../input.txt");
+            int total = 0;
+            foreach (string line in input.Split('\n'))
+            {
+                string newline = toNumbers(line);
+                Console.WriteLine(": " + newline);
+                int digitTotal = getDigitTotal(newline);
+                Console.WriteLine("\t" + digitTotal);
+                total += digitTotal;
+            }
+            Console.WriteLine(total);
+        }
+
+        private static string toNumbers(String line)
+        {
+            Boolean search = true;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                string letter = letters[i];
+                line = line.Replace(letter, (i + 1).ToString());
+            }
+            return line;
+        }
+
+        private static int getDigitTotal(String line)
+        {
+            int linetotal = 0;
+                
+            Boolean search = true;
+            char[] lineArray = line.ToCharArray();
+            int index = 0;
+            while (search)
+            {
+                if (Char.IsNumber(lineArray[index]))
+                {
+                    linetotal += (int.Parse(lineArray[index].ToString()))*10;
+                    search = false;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            search = true;
+            char[] newLineArray = lineArray;
+            Array.Reverse(newLineArray);
+            index = 0;
+            while (search)
+            {
+                if (Char.IsNumber(newLineArray[index]))
+                {
+                    linetotal += int.Parse(newLineArray[index].ToString());
+                    search = false;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            return linetotal;
+        }
+    }
+}
